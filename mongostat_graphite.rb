4ef5d4d477869e086705d8ed6fb5ac3f74d8a753
Mehul Shah <mehul.shah@timgroup.com>
2014-05-15 10:51:34 +0100

    parsing input from mongostat

--- mongostat_graphite.rb
+++ mongostat_graphite.rb
@@ -1,17 +1,43 @@
 #!/usr/bin/ruby
 
 class MongostatGraphite
+  @headers = ["insert",
+              "query",
+              "update",
+              "delete",
+              "getmore",
+              "command",
+              "flushes",
+              "mapped",
+              "vsize",
+              "res",
+              "faults",
+              "lockedv",
+              "idx",
+              "miss",
+              "qr|qw",
+              "ar|aw",
+              "netIn",
+              "netOut",
+              "conn",
+              "time"]
 
   def read_input
    ARGF.each_line { |line|
      if !(line.start_with?("connected"))
        columns = line.split(/\s/).select{|part| part.length > 0}
-       headers = columns.select { |part| part =~ /^[a-z]|[A-Z]/}
-       headers.each { |header| puts header }
+       new_headers = columns.select { |part| part =~ /^[a-z]|[A-Z]/}
+       # @headers.each { |header| puts header }
 
        data = columns.select { |part| part.gsub(/\s+/, "") =~ /^[0-9]/}
 
-       data.each { |item| puts item }
+       # data.each { |item| puts item }
+
+       if (!data.empty?)
+
+         @headers.zip(data).each { |key, value| puts "#{key}, #{value}" }
+
+       end
 
 
      end
