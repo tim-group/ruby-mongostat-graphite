532b1ec2e40de16af6695d7f4960bc6fcc14dd31
Mehul Shah <mehul.shah@timgroup.com>
2014-05-15 09:20:33 +0100

    parsing input from mongostat

--- mongostat_graphite.rb
+++ mongostat_graphite.rb
@@ -3,7 +3,20 @@
 class MongostatGraphite
 
   def read_input
-   ARGF.each_line { |line| puts line }
+   ARGF.each_line { |line|
+     if !(line.start_with?("connected"))
+       columns = line.split(/\s/).select{|part| part.length > 0}
+       headers = columns.select { |part| part =~ /^[a-z]|[A-Z]/}
+       headers.each { |header| puts header }
+
+       data = columns.select { |part| part.gsub(/\s+/, "") =~ /^[0-9]/}
+
+       data.each { |item| puts item }
+
+
+     end
+
+   }
   end
 
 end
